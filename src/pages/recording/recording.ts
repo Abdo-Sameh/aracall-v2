a3770cf017375b1fa4df21b4aa156b1c3ad58917
Abdo-Sameh <abdo.sameh2000@gmail.com>
2017-12-11 21:16:08 +0200

    create recordings dir

--- src/pages/recording/recording.ts
+++ src/pages/recording/recording.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Media, MediaObject } from '@ionic-native/media';
 import { NavController, NavParams } from 'ionic-angular';
+import { File } from '@ionic-native/file';
 // import {Timer} from './itimer';
 
 /**
@@ -9,7 +10,7 @@ import { NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+declare var cordova: any;
 @Component({
   selector: 'page-recording',
   templateUrl: 'recording.html',
@@ -20,12 +21,33 @@ export class RecordingPage {
   recordCallback
   flag = 0;
   public seconds: number = 0;
-  constructor(private media: Media, public navCtrl: NavController, public navParams: NavParams) {
-    if (this.file == null) {
-      this.name = 'file:///storage/emulated/0/Recordings/' + this.createFileName();
-      this.file = this.media.create(this.name);
-    }
+  constructor(public filePath: File, private media: Media, public navCtrl: NavController, public navParams: NavParams) {
+
+
+
+    this.filePath.checkDir('file:///storage/emulated/0/', 'Recordings').then(() => {
+      // Media Directory Exists, do nothing.
+      // alert('Media Storage Loaded');
+      if (this.file == null) {
+        this.name = 'file:///storage/emulated/0/Recordings/' + this.createFileName();
+        this.file = this.media.create(this.name);
+      }
+    }).catch(err => {
+      // alert('Media Storage not found... Creating');
+      this.filePath.createDir('file:///storage/emulated/0/', 'Recordings', false).then(dir => {
+        if (this.file == null) {
+          this.name = 'file:///storage/emulated/0/Recordings/' + this.createFileName();
+          this.file = this.media.create(this.name);
+        }
+        // alert('Media Directory Created!');
+      }).catch(err => {
+        // alert('CRITICAL ERROR. MEDIA STORAGE DIRECTORY FAILED TO BE CREATED:' + err);
+        // alert('Because of this, media files cannot be stored or loaded locally');
+
+      });
+    });
   }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad RecordingPage');
   }
@@ -93,7 +115,7 @@ export class RecordingPage {
     });
   }
 
-  back(){
+  back() {
     this.navCtrl.pop();
   }
 }
